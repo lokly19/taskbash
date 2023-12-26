ffe69c65002ad7335ecd58dbe6a6640e7ef11c7b
lokly19 <152083133+lokly19@users.noreply.github.com>
2023-12-26 23:54:25 +0800

    Update sc_gen.sh

--- sc_gen.sh
+++ sc_gen.sh
@@ -3,4 +3,4 @@ sc_all_5_http.sh;
 sc_all_5_tolkoIP.sh;
 sc_all_5_tolkoURL.sh;
 
-echo 'Attachment' | mail -s 'Attached files' -a /tmp/ipOut.txt -a /tmp/urlOut.txt -a /tmp/httpOut.txt -a /tmp/errorOut.txt lokly19@gmail.com;
+echo 'Attachment' | mail -s 'Attached files to now from ' $(tail -n 1 ./datesc.txt) -a /tmp/ipOut.txt -a /tmp/urlOut.txt -a /tmp/httpOut.txt -a /tmp/errorOut.txt lokly19@gmail.com;
