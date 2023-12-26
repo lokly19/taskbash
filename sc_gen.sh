d5758800f40b892ab4deb4b7f0a0ccb52776f512
lokly19 <152083133+lokly19@users.noreply.github.com>
2023-12-27 01:02:58 +0800

    Update sc_gen.sh

--- sc_gen.sh
+++ sc_gen.sh
@@ -12,3 +12,6 @@ sc_all_5_tolkoIP.sh;
 sc_all_5_tolkoURL.sh;
 
 echo 'Attachment' | mail -s 'Attached files to now from ' $(tail -n 1 ./datesc.txt) -a /tmp/ipOut.txt -a /tmp/urlOut.txt -a /tmp/httpOut.txt -a /tmp/errorOut.txt lokly19@gmail.com;
+
+rm -f $lockFile
+trap - INT TERM EXIT
